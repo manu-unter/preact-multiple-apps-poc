fc327381eb4b31193c1a4767cd6dbfc08e93101e
Manuel Hornung <manuel.hornung.extern@jsmd-group.com>
2019-12-17 21:04:38 +0100

    Add output.jsonpFunction to avoid runtime conflicts caused by lazy-loading

--- preact.config.js
+++ preact.config.js
@@ -1,6 +1,9 @@
 export default (config, env, helpers) => {
   delete config.entry.polyfills;
   config.output.filename = "[name].js";
+  // This is required for not colliding with other possibly lazy-loading applications in the same document
+  // See https://webpack.js.org/configuration/output/#outputjsonpfunction
+  config.output.jsonpFunction = 'wpJsonpMyWidgetWithANonCollidingName';
 
   let { plugin } = helpers.getPluginsByName(config, "ExtractTextPlugin")[0];
   plugin.options.disable = true;
