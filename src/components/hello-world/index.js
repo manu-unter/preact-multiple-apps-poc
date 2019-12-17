4875b4f8574a1aa366f1feaee9e07bd6fa6a131f
Manuel Hornung <manuel.hornung.extern@jsmd-group.com>
2019-12-17 20:56:38 +0100

    Change main component to resemble the targeted use-case

--- src/components/hello-world/index.js
+++ src/components/hello-world/index.js
@@ -1,11 +1,11 @@
 import { h, Component } from "preact";
-import "./style.scss";
+import styles from "./style.scss";
 
 export default class App extends Component {
   render(props) {
     return (
-      <div>
-        <h1 style={{ color: props.color }}>Hello, World!</h1>
+      <div className={styles.overlay}>
+        <h1 style={{ color: props.color }}>I am an overlay rendered by a completely separate preact app</h1>
       </div>
     );
   }
